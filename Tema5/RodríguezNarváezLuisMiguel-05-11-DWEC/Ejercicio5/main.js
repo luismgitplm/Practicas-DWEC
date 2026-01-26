5aa44fe144f77b82d2a0fe016af54a34ca38aa77
Luis Miguel <lrodnar@g.educaand.es>
2026-01-26 17:36:18 +0100

    .

--- Tema5/RodríguezNarváezLuisMiguel-05-11-DWEC/Ejercicio5/main.js
+++ Tema5/RodríguezNarváezLuisMiguel-05-11-DWEC/Ejercicio5/main.js
@@ -1,17 +1,21 @@
 import { num1,num2 } from "./numerosAleatorios.js";
 import { operacionAleatoria } from "./operacionAleatoria.js";
 
+const operacion = operacionAleatoria()
+const numero1 = num1
+const numero2 = num2
+
 document.getElementById("botonNumeros").addEventListener('click', () => {
     document.getElementById("contenidoNumeros").innerHTML = 
-        `Estos son los números: ${num1()} y ${num2()}`
+        `Estos son los números: ${numero1} y ${numero2}`
 })
 
 document.getElementById("botonOperacion").addEventListener('click', () => {
     document.getElementById("contenidoOperacion").innerHTML =
-        `La operación es: ${operacionAleatoria()[0]}`
+        `La operación es: ${operacion[0]}`
 })
 
 document.getElementById("botonResultado").addEventListener('click', () => {
     document.getElementById("contenidoResultado").innerHTML = 
-        `El resultado de la operación con ambos números es: ${operacionAleatoria[1]}`
+        `El resultado de la operación con ambos números es: ${operacion[1]}`
 })
